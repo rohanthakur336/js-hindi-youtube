bd739daa33f0284a75a7053552085f179129db9f
Rohan Thakur <rthakur_be21@thapar.edu>
2025-01-28 15:18:29 +0530

    new changes

---
+++ 04_comparisonOps.js
@@ -0,0 +1,12 @@
+console.log(2 > 1);
+console.log("2" < 1);
+console.log("2" > 1);
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(null == 0); // false  non predictable outputs
+console.log(null === 0); // false
+console.log(null == ""); // false
+console.log(null === ""); // false
+console.log(null == false); // false
+console.log(2 == "2"); // true
