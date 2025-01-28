9f179c4ddc7f5252d0f35c822895d59ae4b49f79
Rohan Thakur <rthakur_be21@thapar.edu>
2025-01-28 11:46:45 +0530

    updates added

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "rohan@gmail.com";
+var accountPassword = "323232";
+accountCity = "Jabalpur";
+let accountState;
+// accountId = 2; "NOT ALLOWED"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
