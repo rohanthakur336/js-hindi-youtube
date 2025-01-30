7478050717b3feb4b9811b76725ad428c4df0517
Rohan Thakur <rthakur_be21@thapar.edu>
2025-01-30 20:25:40 +0530

    change in 01_basics

--- 01_variables.js
+++ 01_variables.js
@@ -1,10 +1,26 @@
-const accountId = 12345;
-let accountEmail = "rohan@gmail.com";
-var accountPassword = "323232";
-accountCity = "Jabalpur";
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
 let accountState;
-// accountId = 2; "NOT ALLOWED"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
 console.log(accountId);
-console.table([accountEmail, accountPassword, accountCity, accountState]);
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
